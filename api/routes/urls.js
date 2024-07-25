8903e0e8337333105e840ff9d810e0cf1cb7a468
Michael <michael.slaw.delta@gmail.com>
2024-07-25 15:08:21 +0300

    feat: add ger request by original url

--- api/routes/urls.js
+++ api/routes/urls.js
@@ -1,9 +1,11 @@
 const router = require("express").Router();
 const swaggerUi = require("swagger-ui-express");
 
-const { getUrls, addUrl } = require("../services/urlServices");
-// const postgreClient = require("../database/connection");
-// const Url = require("../schemas/url");
+const {
+  getUrls,
+  addUrl,
+  getShortUrlByOriginal,
+} = require("../services/urlServices");
 const swaggerDocument = require("./swagger.json");
 
 router.use("/api-docs", swaggerUi.serve);
@@ -29,6 +31,19 @@ router.post("/", async (req, res) => {
     res.send(`Added ${originalUrl} succesfully to db`).status(201);
   } catch (error) {
     console.error("Failed to add new url to the db", error);
+    res.status(500);
+  }
+});
+
+router.get("/", async (req, res) => {
+  try {
+    const url = getShortUrlByOriginal(req.body);
+    if (!url) {
+      res.json(urls.json()).status(200);
+    }
+    res.send("Couldn't find urls matching your request").status(404);
+  } catch (error) {
+    console.error("Failed to find wanted url", error);
     res.status(500);
   }
 });
